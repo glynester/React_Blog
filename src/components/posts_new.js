706038db3d77950f346be64129dbe782646ede7e
Gee <glynester@hotmail.com>
2022-07-13 21:19:16 +0100

    Validation for new post form. createPost action creator. Callback to navigate to homepage once new post successfully made.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 // You can essentially think of this function 'reduxForm' as being identical, or at least very similar to the Connect helper that we've been using from React Redux.
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field){
+    // let { touched, error } = field.meta;
+    let { meta: {touched, error} } = field;
+    const hasDanger=touched&&error?"has-danger":'';
     return (
-      <div className="form-group">
+      <div className={`form-group ${hasDanger}`}>
         <label>{field.label}</label>
         <input 
           className="form-control"
@@ -14,7 +20,11 @@ class PostsNew extends Component {
           // Field input is an object which contains a bunch of different event handlers and a bunch of different props (like on On Change and on Blur, on Focus, it also has the value of the input. ...field.input - we want all of the different properties in this object to be communicated as props to the input tag.    Don't need to write code like onChange={field.input.onChange}
         />
         {/*  property that is automatically placed on the field object that is responsible for handling validation. */}
-        {field.meta.error}
+        <div className="text-help">
+          {/* {field.meta.touched?field.meta.error:''} */}
+          {touched?error:''}
+        </div>
+        {/* We're making use of meta property that's being tracked internally by redux form called 'Touched' - means the user has focused this input and then focused away from it. */}
       </div>
     )
   }
@@ -22,6 +32,13 @@ class PostsNew extends Component {
   onSubmit(values){
     // this === component
     console.log(values);
+    // Whenever we think about saving data or making API requests of any type inside of a Redux application, we always want to be thinking about action creators.
+    
+    // this.props.createPost(values);
+    this.props.createPost(values,()=>{
+      this.props.history.push('/');
+    });
+    // Pass as a callback so that we only navigate back to homepage after post has been created.
   }
 
   render(){
@@ -48,6 +65,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -72,8 +90,18 @@ function validate(values){
 }
 
 // The validate function will be called automatically by redux form whenever a user attempts to submit the form.
+// export default reduxForm({
+//   validate,
+//   form: 'PostsNewForm'
+// })(PostsNew);
+
+// Redux form is already being used in the exact same style as our Connect helper. So how do we combine these two different helpers together?
+
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+  );
 
+// this is how we stack up multiple connect like helpers
